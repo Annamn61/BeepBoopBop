abf0842f3e9bf1607d9e1f2fd15560c871e320bc
Anna <annamnorm61@gmail.com>
2025-01-31 18:35:39 -0800

    Add Big boi calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
 import './App.css'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
 import { BillLocationBoard } from './components/BillLocationBoard/BillLocationBoard'
+import moment from 'moment'
+// import 'react-big-calendar/lib/sass/styles';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment)
 
 function App() {
 
   return (
       <div>
         <BillLocationBoard />
-       
+        
+        <Calendar
+      localizer={localizer}
+      events={[]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500, width: 800 }}
+    />
       </div>
   )
 }
